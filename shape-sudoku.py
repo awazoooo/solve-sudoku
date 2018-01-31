e0ae68daa32b7f8555281684bbea017e60883ce9
yuyaron <yuyaron01@gmail.com>
2018-01-31 11:55:03 +0900

    refactor: change comments in source

--- shape-sudoku.py
+++ shape-sudoku.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ### Z3の結果と元のパズルを引数にとって,わかりやすく表示.
-###Sudokuのサイズは81x81までなら対応
+### Sudokuのサイズは81x81までなら対応
 
 import sys
 import math
 
-#Z3の出力
+# Z3の出力
 res = open("shelloutput",'r')
 
-#Sudokuのサイズ
+# Sudokuのサイズ
 sizetxt = open("info.txt",'r')
 size = sizetxt.readline()
 PUZZLESIZE = (int)(size.replace("puzzlesize=",""))
 
-#元のパズル
+# 元のパズル
 puz = open(sys.argv[1],'r')
 
-#パズルの解のリスト
+# パズルの解のリスト
 ansList = []
 
-#元のパズルのリスト(ansList風)
+# 元のパズルのリスト(ansList風)
 puzzle = []
 
-#Blockのサイズ
+# Blockのサイズ
 BLOCKSIZE = (int)(math.sqrt(PUZZLESIZE))
 
 line = res.readline()
 
-#satかunsatかどうか -> とりあえずエラー処理は強制終了
+# satかunsatかどうか -> とりあえずエラー処理は強制終了
 if line.strip() == "unsat":
     print("This program is unsat.")
     sys.exit()
@@ -36,14 +36,14 @@ elif line.strip() != "sat":
     print("input error")
     sys.exit()
 
-#values といった文字が出るまで捨てる
+# values といった文字が出るまで捨てる
 while True:
     if res.readline().rstrip() == "values":
         break
 
 line = res.readline()
 
-#Z3の結果を読み取る
+# z3の結果を読み取る
 while line.rstrip() != "end":
     line = line.rstrip()
     line = line.replace("(","")
@@ -52,7 +52,7 @@ while line.rstrip() != "end":
     ansList.append(line[1])
     line = res.readline()
 
-#奇数行の出力
+# 奇数行の出力
 def oddLineOutput(i):
     s = "+"
 
@@ -73,7 +73,7 @@ def oddLineOutput(i):
                 s += "    +"
         print s
 
-#偶数行の出力        
+# 偶数行の出力        
 def evenLineOutput(i,puzzleList):
     s = "|"
     l = puzzleList[i*PUZZLESIZE:(i+1)*PUZZLESIZE]
@@ -87,7 +87,7 @@ def evenLineOutput(i,puzzleList):
             s += " "
     print s
 
-#パズルをprint
+# パズルをprint
 def printPuzzle(listOfPuzzle):
     for i in range(PUZZLESIZE):
         oddLineOutput(i)
@@ -95,18 +95,18 @@ def printPuzzle(listOfPuzzle):
     oddLineOutput(PUZZLESIZE)
 
     
-#パズルの読み取り部分    
+# パズルの読み取り部分    
 def vinit():
     i = 0
     for line in puz.readlines():
         j = 0
-        #2桁以上の数値に対応するために一時的に文字を保持
+        # 2桁以上の数値に対応するために一時的に文字を保持
         s = ""
         for cell in line:
-            #改行
+            # 改行
             if cell == '\n':
                 if s != "":
-                    #パズルのサイズが9の場合と，2桁の場合を分ける
+                    # パズルのサイズが9の場合と，2桁の場合を分ける
                     if (len(str(PUZZLESIZE)) != 1) and (len(s) == 1):
                         puzzle.append("0" + s)
                         s = ""
@@ -115,7 +115,7 @@ def vinit():
                         s = ""
                 i += 1
                 
-            #空白
+            # 空白
             elif cell == ' ':
                 if (len(str(PUZZLESIZE))) == 1:
                     puzzle.append(cell)
@@ -123,10 +123,10 @@ def vinit():
                     puzzle.append(" " + cell)
                 j += 1
                 
-            #アンダースコア 
+            # アンダースコア 
             elif cell == '_':
                 if s != "":
-                    #パズルのサイズが9の場合と，2桁の場合を分ける
+                    # パズルのサイズが9の場合と，2桁の場合を分ける
                     if (len(str(PUZZLESIZE)) != 1) and (len(s) == 1):
                         puzzle.append("0" + s)
                         s = ""
@@ -135,23 +135,23 @@ def vinit():
                         s = ""
                     j += 1
                     
-            #数値
+            # 数値
             else:
                 s += cell
                 
-#パズルの読み取り(9x9以下のサイズ専用)
+# パズルの読み取り(9x9以下のサイズ専用)
 def readPuzzle():
     for _ in range(PUZZLESIZE):
         for cell in puz.readline():
             if cell == "" or cell == '' or cell is None:
                 break
-            #アンダースコアor\n
+            # アンダースコアor\n
             elif cell == "_" or cell == "\n":
                 continue
-            #空白
+            # 空白
             elif cell == " ":
                 puzzle.append(cell)
-            #数値
+            # 数値
             else:
                 puzzle.append(str(cell))
 
@@ -164,10 +164,10 @@ print "\n[Solved Puzzle]"
 printPuzzle(ansList)
 print "\n"
 
-#複数解の存在チェック
+# 複数解の存在チェック
 info = open("info.txt","r+a")
 countAnswer = None
-#まず、今までに解があるかを調べる
+# まず、今までに解があるかを調べる
 line = info.readline()
 line = info.readline()
 if line.strip() == "" :
